417207def06c31b73675185a233377312e4b873e
Jack Anderson <andersjack@gmail.com>
2019-09-12 23:08:59 -0600

    connect db to server

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,15 +1,22 @@
 import express from 'express'
 import { ApolloServer } from 'apollo-server-express'
+import { buildFederatedSchema } from '@apollo/federation'
+import { sequelize as db } from 'db/sequelize'
 import typeDefs from '../graphql/typeDefs'
 import resolvers from '../graphql/resolvers'
+;(async () => {
+  await db.sync()
+  const server = new ApolloServer({
+    schema: buildFederatedSchema([{ typeDefs, resolvers }]),
+    context: { db },
+  })
 
-const server = new ApolloServer({ typeDefs, resolvers })
+  const app = express()
+  const port = process.env.port || 3000
 
-const app = express()
-const port = process.env.port || 3000
+  server.applyMiddleware({ app })
 
-server.applyMiddleware({ app })
-
-app.listen(port, () =>
-  console.log(`Typescript template app listening on port ${port}!`),
-)
+  app.listen(port, () =>
+    console.log(`Typescript template app listening on port ${port}!`),
+  )
+})()
